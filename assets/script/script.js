4f8aab8e0442a291339ad6b50faa838324daa795
snow0Js <anaguerra@santacasabh.org.br>
2024-06-27 13:21:42 -0300

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+// validação/formatar cpf
+
+//Salvar dados
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const cpf = document.getElementById('cpf').value;
+    const placa = document.getElementById('placa').value;
+    localStorage.setItem('cpf', cpf);
+    localStorage.setItem('placa', placa);
+    window.location.href = "../views/dados.html"; 
+});
